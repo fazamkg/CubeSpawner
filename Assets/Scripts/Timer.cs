c30d84f658e5d6a414b853c0ad7e00a2063db6b6
Maxim Korovin <fazamkg@gmail.com>
2022-10-11 19:19:04 +0500

    Timer logic

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+	[SerializeField] private bool _isRepeating = true;
+
+	private bool _isDone;
+	private float _timer;
+
 	[field: SerializeField] public float Interval { get; set; }
 
+	private void Update()
+	{
+		if (_isDone) return;
 
+		_timer += Time.deltaTime;
+		if (_timer < Interval) return;
+
+		if (_isRepeating)
+		{
+			_timer = 0.0f;
+		}
+		else
+		{
+			_isDone = true;
+		}
+	}
 }
